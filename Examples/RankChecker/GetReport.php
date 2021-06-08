f25da7f7d159d7de41e6ad6279c64db8c6297ed2
opt1st <john@optfirst.com>
2021-06-08 16:18:01 -0400

    Update GetReport.php

--- Examples/RankChecker/GetReport.php
+++ Examples/RankChecker/GetReport.php
@@ -3,7 +3,7 @@ require '../../vendor/autoload.php';
 
 use BrightLocal\Api;
 
-$api = new Api('<YOUR_API_KEY>', '<YOUR_API_SECRET>');
+$api = new Api('<815b239416b43f8c23f1d78657ceb669c12a8003>', '<55f95e5ee45d0>');
 $response = $api->get('/v2/lsrc/get', [
     'campaign-id' => 50
 ]);
